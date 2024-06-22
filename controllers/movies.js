2c54f2ab9cc5280269ff52438cf7831b12b8187c
James Fagan <jamesfagan95@gmail.com>
2024-06-22 13:31:09 -0400

    Coding structure complete before CSS

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,27 +5,113 @@ const Movie = require(`../models/movies`)
 
 //SEED
 
-router.get("/seed", async (req, res) => {
-    try {
-      await Movie.create([
-        {
-          title: "grapefruit",
-          director: "pink",
-          genre: "[`Action`, `Comedy`, `Horror`, `Drama`, `Fantasy`, `Sci-Fi`, `Documentary`]",
-          rating: 1-10,
-          image: `url`,
-          watchAgain: true,
-        },
+// router.get("/seed", async (req, res) => {
+//     try {
+//       await Movie.create([
+//         {
+//           title: "grapefruit",
+//           director: "pink",
+//           genre: "[`Action`, `Comedy`, `Horror`, `Drama`, `Fantasy`, `Sci-Fi`, `Documentary`]",
+//           rating: 1-10,
+//           image: `url`,
+//           watchAgain: true,
+//         },
         
-      ]);
-      res.redirect("/movies");
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-    }
-  });
+//       ]);
+//       res.redirect("/movies");
+//     } catch (err) {
+//       console.error(err);
+//       res.status(500).send("Internal Server Error");
+//     }
+//   });
 
   //INDUCES
+
+  //INDEX
+
+router.get(`/`, async (req,res) => {
+  try{
+      const allMovies = await Movie.find({})
+      res.render(`index.ejs`, {movies: allMovies })
+  }catch(err) {
+      console.error(err)
+  }
+})
+  
+
+
+//NEW
+
+router.get(`/new`, (req,res) => {
+  res.render(`new.ejs`)
+})
+
+//DELETE
+
+router.delete(`/movies/:id`, async (req,res) => {
+  try{
+      await Movie.findByIdAndDelete(req.params.id)
+      res.redirect(`/movies`)
+  }catch(err) {
+      console.error(err)
+  }
+})
+
+//UPDATE
+
+router.put(`/:id`, async (req,res) => {
+  if(req.body.watchAgain === `on`){
+      req.body.watchAgain = true
+  }else {
+      req.body.watchAgain = false
+  }
+  try {
+      const updateModel = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      res.redirect(`/movies`)
+  }catch(err) {
+      console.error(err)
+  }
+})
+
+//CREATE
+
+router.post(`/`, async (req,res) => {
+  console.log(req.body)
+  if (req.body.watchAgain === `on`) {
+      req.body.watchAgain = true
+  } else {
+      req.body.watchAgain = false
+  }
+  try{
+      const createdMovie = await Movie.create(req.body)
+      res.redirect(`/movies`)
+  }catch(err) {
+      console.error(err)
+  }
+})
+
+//EDIT
+
+router.get('/:id/edit', async (req, res)=>{
+  try {
+      const foundMovie = await Movie.findById(req.params.id)
+      res.render(`edit.ejs`, { movie: foundMovie})
+  }catch(err) {
+      console.error(err)
+  }
+})
+
+
+//SHOW
+
+router.get(`/:id`, async (req,res) => {
+  try {
+      const foundMovie = await Movie.findById(req.params.id)
+      res.render(`show.ejs`, { movie: foundMovie })
+  }catch(err) {
+      console.error(err)
+  }
+})
 
 
 
--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const app = express()
 const mongoose = require(`mongoose`)
 const port = process.env.PORT || 4000
 const methodOverride = require(`method-override`)
-// const moviesControllers = require(`./controllers/movies`)
+const moviesController = require(`./controllers/movies`)
 
 //DATABASE
 
 const Movie = require(`./models/movies`)
-// const Movie = require("./models/movies")
 
 
 //MIDDLEWARE
+app.use(`/movies`, moviesController)
 app.use(express.static(`public`))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride(`_method`))
-// app.use(`/movies`,moviesControllers)
+
 
 const mongoURI = `mongodb+srv://jafbath:ripper11@moviemanager.82w3xlf.mongodb.net/`
 
@@ -33,118 +33,6 @@ connectToMongo()
 
 
 //INDUCES
-
-//SEED
-
-// router.get("/seed", async (req, res) => {
-//     try {
-//       await Movie.create([
-//         {
-//           title: "grapefruit",
-//           director: "pink",
-//           genre: "[`Action`, `Comedy`, `Horror`, `Drama`, `Fantasy`, `Sci-Fi`, `Documentary`]",
-//           rating: 1-10,
-//           image: `url`,
-//           watchAgain: true,
-//         },
-        
-//       ]);
-//       res.redirect("/movies");
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).send("Internal Server Error");
-//     }
-//   });
-
-
-
-
-//INDEX
-
-app.get(`/movies`, async (req,res) => {
-    try{
-        const allMovies = await Movie.find({})
-        res.render(`index.ejs`, {movies: allMovies })
-    }catch(err) {
-        console.error(err)
-    }
-})
-    
-
-
-//NEW
-
-app.get(`/movies/new`, (req,res) => {
-    res.render(`new.ejs`)
-})
-
-//DELETE
-
-app.delete(`/movies/:id`, async (req,res) => {
-    try{
-        await Movie.findByIdAndDelete(req.params.id)
-        res.redirect("/movies")
-    }catch(err) {
-        console.error(err)
-    }
-})
-
-//UPDATE
-
-app.put(`/movies/:id`, async (req,res) => {
-    if(req.body.watchAgain === `on`){
-        req.body.watchAgain = true
-    }else {
-        req.body.watchAgain = false
-    }
-    try {
-        const updateModel = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        res.redirect("/movies")
-    }catch(err) {
-        console.error(err)
-    }
-})
-
-//CREATE
-
-app.post(`/movies`, async (req,res) => {
-    console.log(req.body)
-    if (req.body.watchAgain === `on`) {
-        req.body.watchAgain = true
-    } else {
-        req.body.watchAgain = false
-    }
-    try{
-        const createdMovie = await Movie.create(req.body)
-        res.redirect(`/movies`)
-    }catch(err) {
-        console.error(err)
-    }
-})
-
-//EDIT
-
-app.get('/movies/:id/edit', async (req, res)=>{
-    try {
-        const foundMovie = await Movie.findById(req.params.id)
-        res.render("edit.ejs", { movie: foundMovie})
-    }catch(err) {
-        console.error(err)
-    }
-})
-	
-
-//SHOW
-
-app.get(`/movies/:id`, async (req,res) => {
-    try {
-        const foundMovie = await Movie.findById(req.params.id)
-        res.render("show.ejs", { movie: foundMovie })
-    }catch(err) {
-        console.error(err)
-    }
-})
-
 
 
 //SERVER
