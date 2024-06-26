480df9a29db1abf3ad34eac60f70f272d908893a
James Fagan <jamesfagan95@gmail.com>
2024-06-25 21:35:44 -0400

    touching up formatting of css

--- models/movies.js
+++ models/movies.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema
 const movieSchema = new Schema({
     title: {type: String, required: true},
     director: {type: String, required: true},
-    genre: {type: [String], enum: [`Action`,`Thriller`,`Mystery`, `Comedy`, `Horror`, `Drama`, `Fantasy`, `Sci-Fi`, `Documentary`]},
+    genre: {type: [String], enum: [`Action`,`Thriller`,`Mystery`, `Comedy`, `Horror`, `Drama`, `Fantasy`, `Sci-Fi`, `Documentary`, `Romance`, `Crime`, `Western`]},
     rating: {type: Number, required: true, min: 1, max: 10},
     image: { type: String },
     watchAgain: Boolean
